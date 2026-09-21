a77a8bae421feaf0b31b29183455912cef607b6b
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-22 00:41:13 +0300

    add logger

---
+++ src/logging/logger.c
@@ -0,0 +1,33 @@
+#include "logger.h"
+
+#include <stdio.h>
+
+void logger_init(int level) {
+    if (level >= LOGGER_LEVEL_MIN && level <= LOGGER_LEVEL_MAX) {
+        logger_level = level;
+    }
+    else {
+        logger_level = LOGGER_LEVEL_DISABLED;
+    }
+}
+
+
+void logger_log(int level, const char* string) {
+    if (level >= logger_level) {
+        printf("%s", string);
+    }
+}
+
+
+void logger_debug(const char* string) {
+    logger_log(LOGGER_LEVEL_DEBUG, string);
+}
+void logger_info(const char* string) {
+    logger_log(LOGGER_LEVEL_INFO, string);
+}
+void logger_warn(const char* string) {
+    logger_log(LOGGER_LEVEL_WARN, string);
+}
+void logger_error(const char* string) {
+    logger_log(LOGGER_LEVEL_ERROR, string);
+}
---
+++ src/logging/logger.h
@@ -0,0 +1,30 @@
+#ifndef MYCOOLGAMEENGINE_LOGGER_H
+#define MYCOOLGAMEENGINE_LOGGER_H
+
+
+#define LOGGER_LEVEL_DEBUG 0
+#define LOGGER_LEVEL_INFO 1
+#define LOGGER_LEVEL_WARN 2
+#define LOGGER_LEVEL_ERROR 3
+#define LOGGER_LEVEL_DISABLED 4
+
+#define LOGGER_LEVEL_MIN LOGGER_LEVEL_DEBUG
+#define LOGGER_LEVEL_MAX LOGGER_LEVEL_DISABLED
+
+
+static int logger_level;
+
+
+void logger_init(int level);
+
+
+void logger_log(int level, const char* string);
+
+
+void logger_debug(const char* string);
+void logger_info(const char* string);
+void logger_warn(const char* string);
+void logger_error(const char* string);
+
+
+#endif
