c636bbd0d39f57ec6851f6e6898ff307a70147bc
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-21 23:44:56 +0300

    add vector2

---
+++ src/utils/Vector2.c
@@ -0,0 +1,14 @@
+#include "Vector2.h"
+
+struct Vector2 vector2_0() {
+    struct Vector2 vector;
+    vector.x = 0;
+    vector.y = 0;
+    return vector;
+}
+struct Vector2 vector2_1() {
+    struct Vector2 vector;
+    vector.x = 1;
+    vector.y = 1;
+    return vector;
+}
---
+++ src/utils/Vector2.h
@@ -0,0 +1,12 @@
+#ifndef MYCOOLGAMEENGINE_VECTOR2_H
+#define MYCOOLGAMEENGINE_VECTOR2_H
+
+struct Vector2 {
+    double x;
+    double y;
+};
+
+struct Vector2 vector2_0();
+struct Vector2 vector2_1();
+
+#endif
