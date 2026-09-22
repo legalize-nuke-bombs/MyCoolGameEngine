531c1b08d16e0a2e44c98f3ad5e37c87196e8932
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-22 14:42:51 +0300

    add action

---
+++ src/utils/action.h
@@ -0,0 +1,20 @@
+//
+// Created by nikita on 22.09.2026.
+//
+
+#ifndef MYCOOLGAMEENGINE_ACTION_H
+#define MYCOOLGAMEENGINE_ACTION_H
+
+#include "list.h"
+
+struct action {
+    struct list _list;
+};
+
+void action_init(struct action *this);
+void action_destroy(const struct action *this);
+
+void action_add(struct action *this, void (*action)(void *action_context));
+void action_invoke(const struct action *this, void* action_context);
+
+#endif //MYCOOLGAMEENGINE_ACTION_H
