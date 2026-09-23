6695cd44889ba45e3681a62837e745d121aa2cf7
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-23 17:09:43 +0300

    box_renderer calls pipeline

---
+++ src/rendering/renderer_layers.c
@@ -0,0 +1,10 @@
+//
+// Created by nikita on 23.09.2026.
+//
+
+#include "renderer_layers.h"
+
+const struct renderer_layer renderer_layer_bg1 = {
+    .name = "bg1",
+    .priority = 0
+};
--- src/scene/components/custom/box_renderer.c
+++ src/scene/components/custom/box_renderer.c
@@ -4,15 +4,21 @@
 
 #include "box_renderer.h"
 
-#include <stddef.h>
 #include <stdlib.h>
 
 #include "../component_internal.h"
-#include "../../../logging/logger.h"
+#include "../../../rendering/renderer_pipeline.h"
+#include "../../../rendering/custom/renderer_square.h"
+#include "../../../rendering/renderer_layers.h"
+#include ".../../../../entity.h"
+#include "../../scene.h"
+#include "../../../engine/engine_context.h"
 
 struct box_renderer {
     struct component base;
     struct color color;
+    struct renderer_pipeline* renderer;
+    struct renderer_pipeline_draw_call draw_call;
 };
 
 static const char* box_renderer_component_key(void);
@@ -40,11 +46,19 @@ struct box_renderer* box_renderer_create(struct color color) {
 }
 
 static void box_renderer_awake(struct component *base) {
-    logger_debug("BOX RENDERER AWAKE");
+    struct box_renderer *this = (struct box_renderer *) base;
+
+    this->renderer = scene_get_engine_context(entity_get_parent(component_get_parent(base)))->renderer_pipeline;
+    this->draw_call.primitive = renderer_square_as_renderer_primitive(renderer_square_create(this->color));
+    this->draw_call.layer = renderer_layer_bg1;
+    this->draw_call.rect.position = component_get_local_position(base);
+    this->draw_call.rect.size = component_get_local_scale(base);
 }
 
 static void box_renderer_update(struct component *base, const struct update_context *context) {
-    logger_debug("BOX RENDERER RUN");
+    const struct box_renderer *this = (struct box_renderer *) base;
+
+    renderer_pipeline_draw_primitive(this->renderer, this->draw_call);
 }
 
 struct component * box_renderer_as_component(struct box_renderer *this) {
