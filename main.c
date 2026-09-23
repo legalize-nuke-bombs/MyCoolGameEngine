252a2d0b5e3ded99b6f2aacca814ed1fa267ed57
mr2anderson <cheb0tarev.nikit0s@gmail.com>
2026-09-23 15:53:00 +0300

    test

--- main.c
+++ main.c
@@ -34,18 +34,41 @@ int main(void) {
         .dt = 0
     };
 
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    if (!SDL_CreateWindowAndRenderer("MyCoolGameEngine", 1280, 720, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("SDL_CreateWindowAndRenderer failed: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    SDL_SetRenderVSync(renderer, 1);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
     Uint64 previous = SDL_GetTicksNS();
 
-    for (long long i = 0; i < 100000000ll; i++) {
+    for (long long i = 0; i < 120; i++) {
         const Uint64 now = SDL_GetTicksNS();
-
         update_context.dt = (double)(now - previous) / 1e9;
         previous = now;
 
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {}
+
         scene_update(scene, &update_context);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
 
     scene_destroy(scene);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
