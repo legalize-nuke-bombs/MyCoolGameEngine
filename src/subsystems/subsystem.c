8ab120f395da0350870b5483e4e31246ff96845f
legalize-nuke-bombs <cheb0tarev.nikit0s@gmail.com>
2026-09-25 20:28:30 +0300

    minor code cleanup

--- src/subsystems/subsystem.c
+++ src/subsystems/subsystem.c
@@ -8,13 +8,13 @@
 
 void subsystem_create(struct subsystem *this, const struct subsystem_vtable *vtable, const struct subsystem_collection *subsystems) {
     this->vtable = vtable;
-    logger_info("Subsystem %s is creating...", subsystem_get_name(this));
+    logger_info("Subsystem `%s` is creating...", subsystem_get_name(this));
     this->enabled = false;
     this->subsystems = subsystems;
 }
 void subsystem_destroy(struct subsystem *this) {
     subsystem_disable(this);
-    logger_info("Subsystem %s is destroying...", subsystem_get_name(this));
+    logger_info("Subsystem `%s` is destroying...", subsystem_get_name(this));
     if (this->vtable->on_destroy) {
         this->vtable->on_destroy(this);
     }
@@ -24,7 +24,7 @@ void subsystem_enable(struct subsystem *this) {
     if (this->enabled) {
         return;
     }
-    logger_info("Subsystem %s is enabling...", subsystem_get_name(this));
+    logger_info("Subsystem `%s` is enabling...", subsystem_get_name(this));
     if (this->vtable->on_enable) {
         this->vtable->on_enable(this);
     }
@@ -33,7 +33,7 @@ void subsystem_disable(struct subsystem *this) {
     if (!this->enabled) {
         return;
     }
-    logger_info("Subsystem %s is disabling...", subsystem_get_name(this));
+    logger_info("Subsystem `%s` is disabling...", subsystem_get_name(this));
     if (this->vtable->on_disable) {
         this->vtable->on_disable(this);
     }
